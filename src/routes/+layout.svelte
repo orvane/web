47755edd0853e20afe993237fc4bcffa989578f9
dawidseipold <seipold.dawid@gmail.com>
2024-10-08 12:27:00 +0200

    build: add pandacss

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+	import '../app.css';
+
+	interface MainLayoutProps {
+		children: Snippet;
+	}
+
+	let { children }: MainLayoutProps = $props();
+</script>
+
+{@render children()}
