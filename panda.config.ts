fbfe7899495e73f852c98e3d958188fb57ec7730
dawidseipold <seipold.dawid@gmail.com>
2024-10-08 14:36:58 +0200

    chore: update the path of panda include

--- panda.config.ts
+++ panda.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   preflight: true,
 
   // Where to look for your css declarations
-  include: ["./src/**/*.{js,jsx,ts,tsx}"],
+  include: ['./src/**/*.{js,ts,svelte}'],
 
   // Files to exclude
   exclude: [],
