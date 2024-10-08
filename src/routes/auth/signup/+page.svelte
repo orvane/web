776e73a8ecabe2242457c7e2cbc77c1d8b3752ee
dawidseipold <seipold.dawid@gmail.com>
2024-10-08 23:03:15 +0200

    chore: create empty auth pages and remove the old main page

---
+++ src/routes/auth/signup/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+
+	interface AuthLayoutProps {
+		children: Snippet;
+	}
+
+	let {}: AuthLayoutProps = $props();
+</script>
+
+<div>test</div>
