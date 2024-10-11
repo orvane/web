4831d881854c913ad6d45bcc337812cdf58cd8a7
dawidseipold <seipold.dawid@gmail.com>
2024-10-11 12:48:54 +0200

    feat: add password recovery valibot schema

--- src/lib/valibot/schemas/auth/password-recovery.ts
+++ src/lib/valibot/schemas/auth/password-recovery.ts
@@ -1,6 +1,21 @@
 import * as v from 'valibot'
-import { EmailSchema } from '.'
+import { EmailSchema, PasswordSchema } from '.'
 
 export const PasswordRecoveryRequestSchema = v.object({
   email: EmailSchema
 })
+
+export const PasswordRecoverySchema = v.pipe(
+  v.object({
+    password: PasswordSchema,
+    repeatPassword: v.string()
+  }),
+  v.forward(
+    v.partialCheck(
+      [['password'], ['repeatPassword']],
+      (input) => input.password === input.repeatPassword,
+      "Passwords do not match"
+    ),
+    ['repeatPassword']
+  )
+)
