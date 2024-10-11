e52652f1b9852abcb3c33351cd9f313e514162c7
dawidseipold <seipold.dawid@gmail.com>
2024-10-11 12:48:36 +0200

    fix: modify valibot PasswordSchema regex
    - Simple change from [A-z] to [A-Z] to match only uppercase characters
    as intended

--- src/lib/valibot/schemas/auth/index.ts
+++ src/lib/valibot/schemas/auth/index.ts
@@ -15,7 +15,7 @@ export const PasswordSchema = v.pipe(
   v.minLength(8, "Your password is too short"),
   v.maxLength(128, "Your password is too long"),
   v.regex(/[a-z]/, "Your password must contain a lowercase letter"),
-  v.regex(/[A-z]/, "Your password must contain a uppercase letter"),
+  v.regex(/[A-Z]/, "Your password must contain a uppercase letter"),
   v.regex(/[0-9]/, "Your password must conaint a number")
 )
 
