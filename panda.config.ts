c0cc81db05593535fa2b0234d2ed05723302df0c
dawidseipold <seipold.dawid@gmail.com>
2024-10-12 23:38:45 +0200

    feat(design): create a scaffolding for color scheme creation

--- panda.config.ts
+++ panda.config.ts
@@ -10,9 +10,37 @@ export default defineConfig({
   // Files to exclude
   exclude: [],
 
+  conditions: {
+    light: "[data-color-mode=light] &",
+    dark: "[data-color-mode=dark] &"
+  },
+
   // Useful for theme customization
   theme: {
-    extend: {},
+    extend: {
+      tokens: {
+        colors: {
+          red: {
+            value: '#f72a3f',
+          },
+          darkred: {
+            value: "#6e030e"
+          }
+        }
+      }
+    },
+    semanticTokens: {
+      colors: {
+        neutral: {
+          100: {
+            value: {
+              base: '{colors.red}',
+              _dark: '{colors.darkred}'
+            }
+          }
+        }
+      }
+    }
   },
 
   // The output directory for your css system
