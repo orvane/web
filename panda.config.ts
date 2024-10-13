da0df952dd8df40e72cbd4a777d383fc0e0bd23f
dawidseipold <seipold.dawid@gmail.com>
2024-10-13 21:21:56 +0200

    feat(design-system): add new scaffolding elements for the design system

--- panda.config.ts
+++ panda.config.ts
@@ -17,27 +17,136 @@ export default defineConfig({
 
   // Useful for theme customization
   theme: {
-    extend: {
-      tokens: {
-        colors: {
-          red: {
-            value: '#f72a3f',
+    tokens: {
+      colors: {
+        accent: {
+          purple: {
+            light: {
+              value: "#fff"
+            },
+            dark: {
+              value: "#fff"
+            }
+          }
+        },
+        neutral: {
+          100: {
+            light: {
+              value: '#888888'
+            },
+            dark: {
+              value: '#161616',
+            }
           },
-          darkred: {
-            value: "#6e030e"
+          200: {
+            light: {
+              value: '#fff'
+            },
+            dark: {
+              value: '#fff'
+            }
+          },
+          300: {
+            light: {
+              value: "#fff"
+            },
+            dark: {
+              value: "#fff"
+            }
+          },
+          950: {
+            light: {
+              value: "#123"
+            },
+            dark: {
+              value: "#987"
+            }
           }
         }
       }
+
     },
     semanticTokens: {
       colors: {
-        neutral: {
-          100: {
+        brand: {
+          primary: {
             value: {
-              base: '{colors.red}',
-              _dark: '{colors.darkred}'
+              base: '{colors.accent.pruple.light}',
+              _dark: '{colors.accent.pruple.dark}'
             }
           }
+        },
+        bg: {
+          primary: {
+            value: {
+              base: '{colors.neutral.100.light}',
+              _dark: '{colors.neutral.100.dark}'
+            }
+          },
+          secondary: {
+            value: {
+              base: '{colors.neutral.200.light}',
+              _dark: '{colors.neutral.200.dark}',
+            }
+          },
+          tertiary: {
+            value: {
+              base: '{colors.neutral.300.light}',
+              _dark: '{colors.neutral.300.dark}',
+            }
+          }
+        },
+        fg: {
+          primary: {
+            value: {
+              base: '{colors.neutral.950.light}',
+              _dark: '{colors.neutral.950.dark}'
+            }
+          },
+          secondary: {
+            value: {
+              base: '{colors.neutral.900.light}',
+              _dark: '{colors.neutral.900.dark}',
+            }
+          },
+          tertiary: {
+            value: {
+              base: '{colors.neutral.800.light}',
+              _dark: '{colors.neutral.800.dark}',
+            }
+          },
+          muted: {
+            value: {
+              base: '{colors.neutral.600.light}',
+              _dark: '{colors.neutral.600.dark}',
+            }
+          }
+        },
+        status: {
+          info: {
+            value: {
+              base: '{colors.accent.lightblue.light}',
+              dark: '{colors.accent.darkblue.dark}',
+            }
+          },
+          success: {
+            value: {
+              base: '{colors.accent.lightgreen.light}',
+              dark: '{colors.accent.darkgreen.dark}',
+            }
+          },
+          warning: {
+            value: {
+              base: '{colors.accent.lightorange.light}',
+              dark: '{colors.accent.darkorange.dark}',
+            }
+          },
+          error: {
+            value: {
+              base: '{colors.accent.lightred.light}',
+              dark: '{colors.accent.darkred.dark}',
+            }
+          },
         }
       }
     }
