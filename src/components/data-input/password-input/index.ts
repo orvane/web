d958d0d83855d73c20878519a30a2d3268dbb18b
dawidseipold <seipold.dawid@gmail.com>
2024-10-17 13:03:57 +0200

    refactor(components): add the sva styling, variants, etc. to the password-input

--- src/components/data-input/password-input/index.ts
+++ src/components/data-input/password-input/index.ts
@@ -1,6 +1,53 @@
 import { type FormPathLeaves, type SuperForm } from 'sveltekit-superforms'
 import Root from './password-input.svelte'
 import type { HTMLInputAttributes } from 'svelte/elements'
+import { sva } from 'styled-system/css'
+
+const styles = sva({
+  className: "text-input",
+  slots: ['root', 'control', 'input', 'toggle', 'label', 'description', 'errors', 'error'],
+  base: {
+    root: {
+      display: 'flex',
+      flexDirection: 'column'
+    },
+    control: {
+      display: 'flex',
+      flexDirection: 'column'
+    },
+    input: {
+      borderWidth: 2,
+    }
+  },
+  variants: {
+    size: {
+      sm: {
+        input: {
+          borderRadius: 8,
+          paddingX: 8,
+          paddingY: 4
+        }
+      },
+      md: {
+        input: {
+          borderRadius: 12,
+          paddingX: 12,
+          paddingY: 6
+        }
+      },
+      lg: {
+        input: {
+          borderRadius: 16,
+          paddingX: 16,
+          paddingY: 8
+        }
+      }
+    }
+  },
+  defaultVariants: {
+    size: 'md'
+  }
+})
 
 type Props<T extends Record<string, unknown>> = Omit<HTMLInputAttributes, "form" | "name" | "size"> & {
   form: SuperForm<T>,
@@ -12,5 +59,6 @@ type Props<T extends Record<string, unknown>> = Omit<HTMLInputAttributes, "form"
 
 export {
   Root as PasswordInput,
-  type Props as PasswordInputProps
+  type Props as PasswordInputProps,
+  styles as PasswordInputStyles
 }
--- src/components/data-input/password-input/password-input.svelte
+++ src/components/data-input/password-input/password-input.svelte
@@ -1,10 +1,9 @@
 <script lang="ts" generics="T extends Record<string, unknown>">
 	import { Control, Description, Field, FieldErrors, Label } from 'formsnap';
-	import { input } from 'styled-system/patterns';
 	import { Toggle } from 'bits-ui';
 	import { Eye, EyeOff } from 'lucide-svelte';
 
-	import type { PasswordInputProps } from '.';
+	import { PasswordInputStyles, type PasswordInputProps } from '.';
 
 	let {
 		value = $bindable(),
@@ -16,40 +15,47 @@
 		...props
 	}: PasswordInputProps<T> = $props();
 
+	let classes = PasswordInputStyles({});
+
 	let passwordVisible = $state(false);
 </script>
 
-<Field {form} {name}>
-	<Control let:attrs>
-		{#if label}
-			<Label>{label}</Label>
+<div class={classes.root}>
+	<Field {form} {name}>
+		<Control let:attrs>
+			<div class={classes.control}>
+				{#if label}
+					<Label class={classes.label}>{label}</Label>
+				{/if}
+
+				<input
+					type={passwordVisible ? 'text' : 'password'}
+					bind:value
+					class={classes.input}
+					{...props}
+					{...attrs}
+				/>
+
+				<Toggle.Root bind:pressed={passwordVisible} class={classes.toggle}>
+					{#if passwordVisible}
+						<Eye />
+					{:else}
+						<EyeOff />
+					{/if}
+				</Toggle.Root>
+			</div>
+		</Control>
+
+		{#if description}
+			<Description class={classes.description}>
+				{description}
+			</Description>
 		{/if}
 
-		<input
-			type={passwordVisible ? 'text' : 'password'}
-			bind:value
-			class={input({
-				type: 'password',
-				size
-			})}
-			{...props}
-			{...attrs}
-		/>
-
-		<Toggle.Root bind:pressed={passwordVisible}>
-			{#if passwordVisible}
-				<Eye />
-			{:else}
-				<EyeOff />
-			{/if}
-		</Toggle.Root>
-	</Control>
-
-	{#if description}
-		<Description>
-			{description}
-		</Description>
-	{/if}
-
-	<FieldErrors />
-</Field>
+		<FieldErrors class={classes.errors} let:errors let:errorAttrs>
+			{#each errors as err}
+				<span class={classes.error} {...errorAttrs}>{err}</span>
+			{/each}
+		</FieldErrors>
+	</Field>
+</div>
